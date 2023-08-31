4ae51372bd359bea04db912fdff3a170f3003365
Prathamesh Sahasrabuddhe <prathamesh16020@gmail.com>
2023-08-31 13:41:53 +0530

    CORS Config file added

---
+++ xiaomi-backend/src/main/java/com/application/xiaomi/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.application.xiaomi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // Add your frontend URL here
+                .allowedMethods("GET", "POST") // Add more methods if needed
+                .allowCredentials(true); // Enable sending cookies from frontend
+    }
+}
