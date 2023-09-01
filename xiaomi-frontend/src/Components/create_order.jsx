2900b4a05b9d97c46267747eb257fa2c2aa3abff
gauridahake24 <gauridahake24@gmail.com>
2023-09-01 17:27:55 +0530

    place order and navigation done

--- xiaomi-frontend/src/Components/create_order.jsx
+++ xiaomi-frontend/src/Components/create_order.jsx
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const RepairOrder = () => {
+    const navigate = useNavigate();
     const [Available_quantity, setAvailableQuantity] = useState(0);
     const [part_name, setPartName] = useState('');
     const [Required_quantity, setRequiredQuantity] = useState('');
     const [warehouse_location, setWarehouseLocation] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if (!Required_quantity || !part_name || !warehouse_location ) {
+            console.error("Please fill in all required fields.");
+            setErrorMessage('All fields are necessary! Please enter required data');
+            return ;
+        }
         
-        const newOrder = {Available_quantity,part_name,Required_quantity, warehouse_location }
+        const newOrder = { Available_quantity, part_name, Required_quantity, warehouse_location }
         const formData = new FormData();
         formData.append("Available_quantity", Required_quantity)
         formData.append("part_name", "screen")
         console.log(newOrder)
-        fetch("http://localhost:8080/service/add",{
-            method:"POST",
+        fetch("http://localhost:8080/service/add", {
+            method: "POST",
             // headers:{"Content-Type": "application/json"},
-            body:formData
+            body: formData
 
-        }).then(()=>{
+        }).then(() => {
+            navigate(`/submit`);
             console.log("new order added")
-        } )
+        })
     };
 
     useEffect(() => {
@@ -41,24 +50,26 @@ const RepairOrder = () => {
     return (
         <div className="repair-order-container">
             <h2>Create Order</h2>
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
             <form className="repair-order-form" onSubmit={handleSubmit}>
-                <label>
-                    Part Name:
-                    <input type="text" value={part_name} onChange={(e) => setPartName(e.target.value)} />
-                </label>
-                <label>
-                    Available Quantity: {Available_quantity}
-                </label>
-                <label>
-                    Required Quantity:
-                    <input type="number" value={Required_quantity} onChange={(e) => setRequiredQuantity(e.target.value)} />
-                </label>
-                <label>
-                    Warehouse Location:
-                    <input type="text" value={warehouse_location} onChange={(e) => setWarehouseLocation(e.target.value)} />
-                </label>
-            
-                <button type="submit">Submit</button>
+                <div className="form-group">
+                    <label htmlFor="part_name">Part Name:</label>
+                    <input type="text" id="part_name" value={part_name} onChange={(e) => setPartName(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Available Quantity: {Available_quantity}</label>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="Required_quantity">Required Quantity:</label>
+                    <input type="number" id="Required_quantity" value={Required_quantity} onChange={(e) => setRequiredQuantity(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="warehouse_location">Warehouse Location:</label>
+                    <input type="text" id="warehouse_location" value={warehouse_location} onChange={(e) => setWarehouseLocation(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <button type="submit">Submit</button>
+                </div>
             </form>
         </div>
     );
--- xiaomi-frontend/src/Components/submit_page.jsx
+++ xiaomi-frontend/src/Components/submit_page.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Submit = () => {
+    const Navigate = useNavigate();
+    const handleStartClick = () =>{
+        Navigate("/SC_requirements")
+    };
     
     return (
         <div>
-            <h1>submit page successfully designed</h1>
+            <h1>Your order has been successfully submitted to warehouse</h1>
+            <button btn-btn-primary onClick={handleStartClick}>Dashboard</button>
             
         </div>
     );
